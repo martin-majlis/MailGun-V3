da3f1548ebc8f2ecb218f86c564de0fcaeb6f45e
Martin Majlis <martin@majlis.cz>
2017-12-11 15:43:36 +0100

    Increase version to 0.3.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ tests_require = []
 
 setup(
     name='MailGun-V3',
-    version='0.2.8',
+    version='0.3.1',
     description='Python Wrapper With Fluent API for Mailgun v3',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
